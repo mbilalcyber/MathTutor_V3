e04711f86b46dd8062f6c7382e04818853287e51
Jacob Lilly (WSC) <jacoblilly1@icloud.com>
2025-10-17 11:55:34 -0500

    fixed issues with levels but play again proped is still broken

--- main.cpp
+++ main.cpp
@@ -26,10 +26,6 @@
 #include <cctype>    // tolower
 using namespace std;
 
-// constants
-const int MAX_ATTEMPTS = 3;         // User has 3 tries per question
-const int LEVEL_RANGE_CHANGE = 10;  // The jump between number
-
 // enum for math type
 enum MathType { MT_ADD = 1, MT_SUB, MT_MUL, MT_DIV };
 
@@ -49,13 +45,25 @@ int main() {
     srand(static_cast<unsigned>(time(0)));
 
     // state variables
+    int leftNum = 0;
+    int rightNum = 0;
+
+    char mathSymbol = '?';
+
+    int correctAnswer = 0;
+    int userAnswer = 0;
+
+    int temp = 0;
     int totalCorrect = 0;
     int totalIncorrect = 0;
-    int mathLevel = 1;                        // start at level 1
-    int levelRange = LEVEL_RANGE_CHANGE;      // current number range (1..levelRange)
-    string userInput = "?";                   // for y/yes/n/no
-    string userName;
+    int currentLevel = 1;
+    int currentRange = 1;
 
+    string userInput = "?";
+    string userName = "?";
+
+    const int maxAttemps = 3;     // User has 3 tries per question
+    const int levelRange = 10;    // The jump between number range for levels
 
     cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -       " << endl;
     cout << "     __  __       _   _       _____      _                          " << endl;
@@ -69,15 +77,15 @@ int main() {
     cout << endl;
 
     getline(cin, userName);
-    if (userName.empty()) userName = "Friend";
+    if (userName.empty()) userName = "User";
     cout << "Welcome " << userName << " to the Silly Simple Math Tutor!\n\n";
 
 
-// main question loop
+    // main question loop
     do {
         // generate numbers based on current range
-        int leftNum  = (rand() % levelRange) + 1;
-        int rightNum = (rand() % levelRange) + 1;
+        int leftNum = (rand() % currentRange) + 1;
+        int rightNum = (rand() % currentRange) + 1;
 
         // choose a random math type (enum)
         MathType mathType = static_cast<MathType>((rand() % 4) + 1);
@@ -119,18 +127,19 @@ int main() {
                 // if the program cant pick, it will give them this error code
             default:
                 cout << "Error: Contact Jacob or Bilal." << endl;
-                cout << "This shouldnt happen :(." << endl;
+                cout << "This shouldnt happen :(" << endl;
                 return -1;
         }
         // allows user 3 attempts to give correct answer and tells them the level they are on
+        // It also asks them the question
         for (int i=1; i<= maxAttemps; i++) {
-            cout << "[Level " << level << "] " << userName << "what is "
+            cout << "[Level " << currentLevel << "] " << userName << "what is "
                     << leftNum << " " << mathSymbol << " " << rightNum << " equal?" << endl;
             cout << "Your answer: ";
             cin >> userAnswer;
 
             // checks if response is valid
-            while (!(cin >> userInput)) {
+            while (!(cin >> userAnswer)) {
                 cin.clear();
                 cin.ignore(numeric_limits<streamsize>::max(),'\n');
                 cout << "Please enter a number:"   << endl; // tells user to give it a number if it was invalid
@@ -145,40 +154,44 @@ int main() {
             //if incorrect, it'll say incorrect and break
             if (i >= maxAttemps) {
                 cout << "Incorrect. The correct answer was " << correctAnswer << endl;
+                totalIncorrect++;
             }
 
             // Tells them how many attempts are left
             else {
                 cout << "You have " << maxAttemps - i << " attempts left" << endl;
+                cout << "Dont give up!" << endl;
             }
+        } // ends user attempts
+
+
+
+        // checks for 3 correct answers then increases level up
+        // it will increase the range to add extra 10 numbers
+        if (totalCorrect == 3) {
+            currentLevel++;
+            totalCorrect = 0;   // new level means clen record
+            totalIncorrect = 0; // goes back to defaults
+            currentRange += levelRange;
+            cout << "Congrats, you leveled up!" << endl;
+            cout << "Your new level is " << currentLevel << " with numbers ranging from 1 to " << currentRange << endl;
         }
-    } // ends user attempts
 
-    // checks for 3 correct answers then increases level up
-    // it will increse the range to add extra 10 numbers
-    if (totalCorrect == 3) {
-        level ++;
-        totalCorrect = 0;
-        totalIncorrect = 0;
-        currentRange += currentRange;
-        cout << "Congrats, you leveled up!" << endl;
-        cout << "Your new level is " << level << " with numbers ranging from 1 to " << currentRange << endl;
+        // checks for 3 wrong answers then lowers their level if its above level 1
+        if (totalIncorrect >= 3 && currentLevel > 1) {
+            currentLevel --;
+            totalCorrect = 0;      // resets to 0
+            totalIncorrect = 0;    // back to defaults
+            currentRange -= levelRange;
+            if (currentRange < levelRange) currentRange = levelRange; //
+            cout << "You leveled down. You're now on Level " << currentLevel
+                 << " with numbers ranging from 1 to " << currentRange << endl;
+
+        } // end of main question loop
+
+    cout << "Thanks for playing, " << userName << "!" << endl;
+    return 0;
+
+        return 0;
     }
-
-    // checks for 3 wrong answers then lowers their level if its above level 1
-    if (totalIncorrect == 3 && level > 1) {
-        level --;
-        totalCorrect = 0;
-        totalIncorrect = 0;
-        currentRange -= currentRange;
-        cout << "You leveled down. Your new level is" << level
-        << "with numbers ranging from 1 to" << currentRange << endl;
-    }
-
-
-
-
-                cout << "Thanks for playing, " << userName << "!" << endl;
-
-            return 0;
-        }
+}
